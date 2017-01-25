87992023b220d53fa90863ddb37bd0e7eefc7bc2
Dramelac <203697@supinfo.com>
2017-01-25 11:44:13 +0100

    Add Tomcat lib

---
+++ src/com/supinfo/supsale/servlet/IndexServlet.java
@@ -0,0 +1,15 @@
+package com.supinfo.supsale.servlet;
+
+import java.io.IOException;
+
+
+@javax.servlet.annotation.WebServlet(name = "IndexServlet")
+public class IndexServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
