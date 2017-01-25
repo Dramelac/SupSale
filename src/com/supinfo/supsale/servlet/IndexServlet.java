53126bba3088a4a0ec645e073412892702865a5f
Dramelac <203697@supinfo.com>
2017-01-25 12:01:11 +0100

    Fix urlPattern

--- src/com/supinfo/supsale/servlet/IndexServlet.java
+++ src/com/supinfo/supsale/servlet/IndexServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@WebServlet(name = "IndexServlet", urlPatterns = "index")
+@WebServlet(name = "IndexServlet", urlPatterns = "/index")
 public class IndexServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
