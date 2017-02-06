c1349ad58de60459028d51118c5eab367baad3f7
Dramelac <203697@supinfo.com>
2017-02-06 23:19:37 +0100

    Fix void register

--- src/com/supinfo/supsale/servlet/user/RegisterServlet.java
+++ src/com/supinfo/supsale/servlet/user/RegisterServlet.java
@@ -4,6 +4,7 @@ import com.supinfo.supsale.DAL.UserDAO;
 import com.supinfo.supsale.entity.User;
 import com.supinfo.supsale.utils.SecurityUtils;
 
+import javax.persistence.RollbackException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +16,7 @@ import java.io.IOException;
 @WebServlet(name = "RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("wrongpassword", false);
+        request.setAttribute("failed", false);
         User user = new User();
         user.setFirstname(request.getParameter("firstname"));
         user.setLastname(request.getParameter("lastname"));
@@ -25,7 +26,7 @@ public class RegisterServlet extends HttpServlet {
         user.setUsername(request.getParameter("username"));
 
 
-        if (request.getParameter("password").equals(request.getParameter("passwordverif"))){
+        if (request.getParameter("password").equals(request.getParameter("passwordverif")) && !request.getParameter("username").isEmpty()){
 
             user.setPassword(SecurityUtils.getHashfromPassword(request.getParameter("password")));
 
@@ -35,14 +36,19 @@ public class RegisterServlet extends HttpServlet {
                 request.getSession().setAttribute("userId", user.getId());
                 request.getSession().setAttribute("isAdmin", user.isAdmin());
                 response.sendRedirect(request.getContextPath() + "/index");
-            } catch (Exception e) {
+            } catch (RollbackException e) {
                 request.setAttribute("failed", true);
+                request.setAttribute("message", "This account or email already exist.");
                 doGet(request, response);
             }
+        } else if (request.getParameter("username").isEmpty()){
+            request.setAttribute("failed", true);
+            request.setAttribute("message", "Required field is missing.");
+            doGet(request, response);
         }
         else {
-            request.setAttribute("wrongpassword", true);
-            request.setAttribute("message2", "Password not match.");
+            request.setAttribute("failed", true);
+            request.setAttribute("message", "Password not match.");
             doGet(request, response);
         }
 
